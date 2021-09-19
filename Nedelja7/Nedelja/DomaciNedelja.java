be51e6a3d002c016a7f7a01960ca8eca7f91c5cc
Danilo Ašanin <88410629+dvasanin@users.noreply.github.com>
2021-09-19 13:13:17 +0200

    Update DomaciNedelja.java

--- Nedelja7/Nedelja/DomaciNedelja.java
+++ Nedelja7/Nedelja/DomaciNedelja.java
@@ -1,3 +1,27 @@
+//Required imports
+
+/*
+import rs.itbootcamp.dao.MealDao;
+import rs.itbootcamp.dao.MealDaoSQL;
+import rs.itbootcamp.model.MealModel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+*/
+
+//Required for testing
+
+/*
+public static void main(String[] args) {
+    static MealDao md = new MealDaoSQL();
+    
+    Scanner sc = new Scanner(System.in);
+    String string = sc.nextLine();
+    search(string);
+}
+*/
+
 public static void search(String string) {
         List<MealModel> allMeals = md.getAllMeals();
         List<String> searchQuery = new ArrayList<>();
@@ -13,4 +37,4 @@ public static void search(String string) {
             }
         }
         System.out.println(searchQuery);
-    }
+}
